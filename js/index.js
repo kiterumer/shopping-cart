94d40a46fa888ddb631ef107de9728c42da49f8f
kiterumer <1404646092@qq.com>
2018-08-29 10:45:57 +0800

    a shopping cart eg

---
+++ js/index.js
@@ -0,0 +1,106 @@
+window.onload=function(){
+	// console.log("hello world");
+	var shop=document.querySelector(".shop"),  //整个购物车
+	    checkAll=shop.querySelector(".checkAll"),  //全选框
+	    check=shop.querySelectorAll(".check"),   //所有单选框
+	    len=check.length,   
+      index=0;
+      // 全选与反选
+      checkAll.onclick=function(){
+      	for(var i=0;i<len;i++){
+          if(checkAll.checked){
+            check[i].checked=true;
+          }else{
+            check[i].checked=false;
+          }
+        }
+        paymentMoney();
+      }
+        // 单选框影响全选框  变量的取值与作用域问题，函数封装的问题
+        for(var i=0;i<len;i++){
+        	check[i].onclick=function(){
+        		if(this.checked){
+        			index++;
+              if(index==len){
+                checkAll.checked=true;
+                }
+        			}else {                  
+              index--;
+              checkAll.checked=false;
+            }
+            paymentMoney();
+          }
+        }
+        		
+    // 单行商品金额计算  重复代码较多
+    var shopItem=document.querySelectorAll('.shop-item'),   //单行商品
+        shopAccounts=document.querySelector('.shop-accounts-money'),  //应付总金额 
+        unitPrice=document.querySelectorAll('.unitPrice-money'), //单价
+        countPrice=document.querySelectorAll('.countPrice'), //总价
+        amount=document.querySelectorAll('.amount'),  //数量
+        add=document.querySelectorAll('.add'),   //加法按钮
+        subtract=document.querySelectorAll('.subtract'); //减法按钮
+         
+        // 点击加减按钮，增减商品数量，计算单行商品总价   重复代码较多
+        for(var a=0;a<len;a++){
+          add[a].title=a;
+          add[a].onclick=function(){
+            index=this.title;
+            amount[index].value=parseInt(amount[index].value)+1;
+            countPrice[index].innerHTML=parseInt(unitPrice[index].innerHTML)*parseInt(amount[index].value);
+            paymentMoney();
+          }
+          subtract[a].title=a;
+          subtract[a].onclick=function(){
+            index=this.title;
+            if(amount[index].value == 1){
+                    amount[index].value = 1;
+                }else{
+            amount[index].value = parseInt(amount[index].value)-1;
+                }
+            countPrice[index].innerHTML=parseInt(unitPrice[index].innerHTML)*parseInt(amount[index].value);
+            paymentMoney();
+          }
+          amount[a].title=a;
+          amount[a].onblur=function(){
+            index=this.title;
+            countPrice[index].innerHTML=parseInt(unitPrice[index].innerHTML)*parseInt(amount[index].value);
+            paymentMoney();
+          }
+        }
+
+     // 应付总金额计算   
+        function paymentMoney(){
+          var num=0;
+        	for(var m=0;m<len;m++){
+            if(check[m].checked){
+               num+= +countPrice[m].innerHTML;
+            }
+          }
+          shopAccounts.innerHTML=num;
+        };
+
+
+       // 微信二维码显示 利用立即执行函数，下同
+        (function(){
+        	var weChat=document.querySelector(".footer-icon-weixin"),
+        	    codePic=document.querySelector(".footer-icon-weixin_QRcode");
+            	weChat.onmouseover=function(){
+            		codePic.style.display="block";
+            	};
+            	weChat.onmouseout=function(){
+            		codePic.style.display="none";
+            	}    
+        })();
+       // 搜索框显示
+       (function(){
+          var seek=document.querySelector(".seek-txt"),
+              placeholder=document.querySelector(".placeholder");
+              seek.onfocus=function(){
+              	placeholder.style.display="none";
+              };
+              seek.onblur=function(){
+                placeholder.style.display="block";
+              };
+       })();
+    }
